75a3c6bf8c68b67c196ff880aad8e2cf2deae202
Ildar Bikmamatov <vistoyn@gmail.com>
2018-06-10 13:48:50 +0000

    init

---
+++ rules.js
@@ -0,0 +1,19 @@
+var bayrell_rtl = require('bayrell_rtl');
+var rtl = bayrell_rtl.rtl;
+
+
+base_path = '';
+if (typeof __dirname != 'undefined') base_path = rtl.clone(__dirname);
+else if (typeof global.__dirname != 'undefined') base_path = rtl.clone(global.__dirname);
+
+
+module.exports = {
+	'type': 1,
+	'base_path': base_path,
+	'files': [
+		'FileInstance',
+		'FileSystemProvider',
+		'ModuleDescription',
+	],
+};
+
